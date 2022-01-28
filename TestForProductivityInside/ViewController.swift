71f6ca44e126e26be881103cde92db0decea66e6
George <george.popandopulo@gmail.com>
2022-01-28 18:36:36 +0300

    Initial Commit

---
+++ TestForProductivityInside/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestForProductivityInside
+//
+//  Created by Георгий Попандопуло on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
